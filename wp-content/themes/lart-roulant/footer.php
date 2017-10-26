e4958cc1e6190e74d059e2573f848a6100652fb0
Kieran Reynolds <reynoldsktsr@gmail.com>
2017-10-26 00:00:00 -0700

    First

---
+++ wp-content/themes/lart-roulant/footer.php
@@ -0,0 +1,12 @@
+			<footer class="footer" role="contentinfo">
+				<p class="copyright">
+					&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>.
+				</p>
+			</footer>
+			<div style="clear:both;"></div>
+		</div>
+		<!-- /wrapper -->
+
+		<?php wp_footer(); ?>
+	</body>
+</html>
---
+++ wp-content/themes/lart-roulant/template-single.php
@@ -0,0 +1,17 @@
+<?php /* Template Name: Demo Page Template */ get_header(); ?>
+
+	<main role="main">
+		<section>
+			<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<?php the_content(); ?>
+				</article>
+			<?php endwhile; ?>
+			<?php else: ?>
+				<article>
+					<h2>Sorry, nothing to display.</h2>
+				</article>
+			<?php endif; ?>
+		</section>
+	</main>
+<?php get_footer(); ?>
