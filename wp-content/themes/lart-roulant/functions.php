162511b8534283cae1f7eea536159fdc046fe5ab
Kieran Reynolds <reynoldsktsr@gmail.com>
2017-11-15 22:05:37 -0800

    Removed change

--- wp-content/themes/lart-roulant/functions.php
+++ wp-content/themes/lart-roulant/functions.php
@@ -15,9 +15,7 @@ if (function_exists('add_theme_support'))
     add_image_size('medium', 250, '', true); // Medium Thumbnail
     add_image_size('small', 120, '', true); // Small Thumbnail
     add_image_size('custom-size', 700, 200, true); // Custom Thumbnail Size call using the_post_thumbnail('custom-size');
-    @ini_set( 'upload_max_size' , '64M' );
-    @ini_set( 'post_max_size', '64M');
-    @ini_set( 'max_execution_time', '300' );
+    
     // Add Support for Custom Backgrounds - Uncomment below if you're going to use
     /*add_theme_support('custom-background', array(
 	'default-color' => 'FFF',
